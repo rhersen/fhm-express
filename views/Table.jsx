137757c3d28684de858463186c09823c857e508b
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-02-26 13:33:38 +0100

    sort columns

--- views/Table.jsx
+++ views/Table.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export const Table = ({ headers, dates, columns, population, f, color }) => {
   const populationValues = Object.values(population);
+
+  const columnMapping = getColumnMapping();
+
   return (
     <div
       className="table"
@@ -21,14 +24,23 @@ export const Table = ({ headers, dates, columns, population, f, color }) => {
         ))}
       {columns.map((column, colIndex) => (
         <>
-          <span className="header">{headers[colIndex]}</span>
+          <span
+            className="header"
+            style={{ gridColumn: columnMapping[colIndex] + 2 }}
+          >
+            {headers[colIndex]}
+          </span>
           {column
             .map((cell, rowIndex) => {
               const a = column.slice(rowIndex - 13, rowIndex + 1);
               const x = f(a, populationValues[colIndex]);
               if (x === undefined) return <span> </span>;
               return (
-                <span key={rowIndex} className={color(x)}>
+                <span
+                  key={rowIndex}
+                  className={color(x)}
+                  style={{ gridColumn: columnMapping[colIndex] + 2 }}
+                >
                   {Math.round(x)}
                 </span>
               );
@@ -39,4 +51,22 @@ export const Table = ({ headers, dates, columns, population, f, color }) => {
       ))}
     </div>
   );
+
+  function getColumnMapping() {
+    return columns
+      .map((column, originalColumnIndex) =>
+        Math.round(f(column.slice(-14), populationValues[originalColumnIndex]))
+      )
+      .map((value, originalColumnIndex) => [originalColumnIndex, value])
+      .sort(([, value1], [, value2]) => value2 - value1)
+      .map(([originalColumnIndex], newColumnIndex) => [
+        originalColumnIndex,
+        newColumnIndex,
+      ])
+      .sort(
+        ([originalColumnIndex1], [originalColumnIndex2]) =>
+          originalColumnIndex1 - originalColumnIndex2
+      )
+      .map(([, newColumnIndex]) => newColumnIndex);
+  }
 };
